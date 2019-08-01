596f0a6e4fed524466f5534605e55b84dc5486ce
Wakka <wakkaflocka239@gmail.com>
2019-08-01 00:53:00 -0500

    Create hat.java

---
+++ src/main/java/com/spleefleague/commands/commands/hat.java
@@ -0,0 +1,50 @@
+import com.spleefleague.annotations.Endpoint;
+import com.spleefleague.annotations.PlayerArg;
+import com.spleefleague.annotations.LiteralArg;
+import com.spleefleague.core.plugin.CorePlugin;
+import com.spleefleague.commands.command.BasicCommand;
+import com.spleefleague.core.player.Rank;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
+public class hat extends BasicCommand {
+
+  public hat(CorePlugin plugin, String name, String usage) {
+    super(plugin, new hatDispatcher(), name, usage, Rank.MODERATOR);
+  }
+
+  @Endpoint
+  public void hat(Player p) {
+    final PlayerInventory inv = p.getInventory();
+    final ItemStack hand = inv.getItemInMainHand();
+    final ItemStack head = inv.getHelmet();
+    if(inv.getItemInMainHand().getType() != Material.AIR) {
+      inv.setItemInMainHand(head);
+      inv.setHelmet(hand);
+      success(p, "Enjoy your new hat!");
+    }else if(head != null && inv.getItemInMainHand().getType() == Material.AIR) {
+      inv.setHelmet(new ItemStack(Material.AIR));
+      inv.setItemInMainHand(head);
+      success(p, "Your hat has been removed!");
+    }else{
+      error(p, "You must be holding something!");
+    }
+  }
+
+  @Endpoint
+  public void hat(Player p, @LiteralArg(value = "remove") String l) {
+    final PlayerInventory inv = p.getInventory();
+    final ItemStack head = inv.getHelmet();
+    if(head == null || head.getType() == Material.AIR){
+      error(p, "You are not wearing a hat!");
+      return;
+    }
+    inv.setHelmet(new ItemStack(Material.AIR));
+    inv.setItemInMainHand(head);
+    success(p, "Your hat has been removed!");
+  }
+}
